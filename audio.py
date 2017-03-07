322790624141156105d53d7eb9ba30c58e714d09
David Suh <suhdavid@uw.edu>
2017-03-07 12:15:40 -0800

    add GPIO controls

--- audio.py
+++ audio.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 
 class audio:
 
-	def audio_setup():
+	def audio_setup(self):
 		GPIO.setmode(GPIO.BCM)
 
 		# bluetooth control GPIO setup
@@ -20,35 +20,35 @@ class audio:
 		GPIO.output(20, 0)
 		GPIO.output(21, 1)
 
-	def bt_volume_up():
+	def bt_volume_up(self):
 		GPIO.output(5, 0)
 		GPIO.output(5, 1)
 
-	def bt_volume_down():
+	def bt_volume_down(self):
 		GPIO.output(26, 0)
 		GPIO.output(26, 1)
 
-	def bt_prev_track():
+	def bt_prev_track(self):
 		GPIO.output(6, 0)
 		GPIO.output(6, 1)
 
-	def bt_next_track():
+	def bt_next_track(self):
 		GPIO.output(19, 0)
 		GPIO.output(19, 1)
 
-	def bt_play_pause():
+	def bt_play_pause(self):
 		GPIO.output(13, 0)
 		GPIO.output(13, 1)
 
-	def system_en():
+	def system_en(self):
 		GPIO.output(20, 0)
 		GPIO.output(21, 1)
 
-	def radio_en():
+	def radio_en(self):
 		GPIO.output(20, 0)
 		GPIO.output(21, 0)
 
-	def bt_en():
+	def bt_en(self):
 		GPIO.output(20, 1)
 		GPIO.output(21, 0)
 
